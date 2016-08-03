af564c4083fc3da80532b771076c035eb614c9bb
foxgr <grf@fcbtech.com.au>
2016-08-03 16:27:12 +1000

    Added SDL2 Hello World framework

---
+++ README.md
@@ -0,0 +1,12 @@
+rx
+==
+
+Uses SDL https://www.libsdl.org/download-2.0.php
+
+on OSX need to follow
+http://stackoverflow.com/questions/10630929/building-sdl2-on-os-x-10-7-ranlib-file-has-no-symbols
+
+Eclipse args
+-I/Library/Frameworks/SDL2.framework/Headers -F/Library/Frameworks -framework SDL2
+
+Plus usual library stuff
--- src/rx.cpp
+++ src/rx.cpp
@@ -5,7 +5,7 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+#include <SDL.h>
 #include <iostream>
 #include "Node.h"
 #include "runner/Runner.h"
@@ -14,29 +14,69 @@
 
 using namespace std;
 
-Node* entrypoint;
-
-void create() {
-	Node *node1 = new Node(1);
-	Node *node2 = new Node(2);
-	Node *node3 = new Node(3);
-
-	node1->setTrue(node2);
-	node1->setFalse(node3);
-
-	node2->setTrue(node3);
-
-	node3->setFalse(node2);
-
-	entrypoint = node1;
-}
-
-int main() {
+int runner() {
 	std::srand(time(0)); // use current time as seed for random generator
 	Runner *runner = new Runner();
 	runner->registerAcp(new Blue(1));
 	runner->registerAcp(new Blue(2));
 	//runner->registerAcp(new Red(3));
 	runner->run();
+	return 0;
+}
+
+/*
+ * Lesson 0: Test to make sure SDL is setup properly
+ */
+int main(int, char**){
+	if (SDL_Init(SDL_INIT_VIDEO) != 0){
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	if (win == nullptr){
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr){
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	std::string imagePath = "src/hello.bmp";
+	SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
+	if (bmp == nullptr){
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+	SDL_FreeSurface(bmp);
+	if (tex == nullptr){
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
+	for (int i = 0; i < 3; ++i){
+		//First clear the renderer
+		SDL_RenderClear(ren);
+		//Draw the texture
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		//Update the screen
+		SDL_RenderPresent(ren);
+		//Take a quick break after all that hard work
+		SDL_Delay(1000);
+	}
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
 	return 0;
 }
